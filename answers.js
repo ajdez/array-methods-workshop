5bfcbee7aa008296fbff28b446fc1be1c6542407
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-12 19:44:19 +0000

    only the pos complete

---
+++ answers.js
@@ -0,0 +1,12 @@
+// new answers.js file
+ 
+//fucnton Print Postivies
+
+function printPositives(array){
+    
+    array.forEach(function(item){
+        if (item > 0){
+            console.log(item);
+        }
+    });
+}
